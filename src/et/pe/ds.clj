bc564a85f4f90d9ed0ea273f40acd90ae8a774c6
Dan <dan@eighttrigrams.net>
2024-12-19 19:19:38 +0000

    Remove namespacing

--- src/et/pe/ds.clj
+++ src/et/pe/ds.clj
@@ -17,29 +17,33 @@
      (with-open [node (start-in-memory-node)]
        (xt/status node))))
 
+(defn- convert-person [{name :xt/id email :person/email}]
+  {:name name :email email})
+
 (defn get-person-by-name-or-email 
   "@returns a person if either the given name or the email match" 
   [node name email]
-  (xt/q node 
-        '(-> (from :persons [xt/id person/email])
-             (where (or (= xt/id $name)
-                        (= person/email $email))))
-        {:args {:name name
-                :email email}}))
+  (map convert-person (xt/q node 
+                     '(-> (from :persons [xt/id person/email])
+                          (where (or (= xt/id $name)
+                                     (= person/email $email))))
+                     {:args {:name  name
+                             :email email}})))
 
 (defn get-person-by-name 
   [node name]
-  (first (xt/q node 
-               '(-> (from :persons [xt/id person/email])
-                    (where (= xt/id $name)))
-               {:args {:name name}})))
+  (convert-person (first (xt/q node 
+                        '(-> (from :persons [xt/id person/email])
+                             (where (= xt/id $name)))
+                        {:args {:name name}}))))
 
 (defn get-person-by-email 
   [node email]
-  (first (xt/q node 
-               '(-> (from :persons [xt/id person/email])
-                    (where (= person/email $email)))
-               {:args {:email email}})))
+  (convert-person
+   (first (xt/q node 
+                '(-> (from :persons [xt/id person/email])
+                     (where (= person/email $email)))
+                {:args {:email email}}))))
 
 (defn add-person 
   "@returns true if person added, false otherwise"
@@ -50,18 +54,20 @@
                                               :person/email email}]])))
 
 (defn list-persons [node]
-  (xt/q node '(from :persons [xt/id person/email])))
+  (map convert-person (xt/q node '(from :persons [xt/id person/email]))))
 
 (defn list-identities [node {person-id :xt/id :as _mind}]
-  (xt/q node 
-        '(-> (from :identities [identity/mind-id identity/text])
-             (where (= identity/mind-id $person-id))
-             (return identity/text))
-        {:args {:person-id person-id}}))
+  (map
+   (fn [{id :xt/id text :identity/text}] {:identity id :text text})
+   (xt/q node 
+         '(-> (from :identities [identity/mind-id identity/text xt/id])
+              (where (= identity/mind-id $person-id))
+              (return identity/text xt/id))
+         {:args {:person-id person-id}})))
 
 (defn add-identity
   "@param mind - person the identity belongs to"
-  [node {person-id :xt/id :as _mind} id text]
+  [node {person-id :name :as _mind} id text]
   (xt/execute-tx node [[:put-docs :identities {:xt/id            id 
                                                :identity/mind-id person-id
                                                :identity/text    text}]]))
--- test/et/pe/ds_test.clj
+++ test/et/pe/ds_test.clj
@@ -19,31 +19,31 @@
     (ds/add-person node :dan2 "d2@et.n")
     (testing "retrieve persons"
       (are [expected actual] (= expected actual)
-        (set [{:xt/id        :dan
-               :person/email "d@et.n"}
-              {:xt/id        :dan2
-               :person/email "d2@et.n"}])
+        (set [{:name :dan
+               :email "d@et.n"}
+              {:name :dan2
+               :email "d2@et.n"}])
         (set (ds/list-persons node))
-        {:xt/id        :dan
-         :person/email "d@et.n"}
+        {:name :dan
+         :email "d@et.n"}
         (ds/get-person-by-name node :dan)
-        {:xt/id        :dan2
-         :person/email "d2@et.n"}
+        {:name :dan2
+         :email "d2@et.n"}
         (ds/get-person-by-email node "d2@et.n")))))
 
 (deftest identities
   (with-open [node (ds/start-in-memory-node)]
     (ds/add-person node :dan "d@et.n")
     (ds/add-person node :dan2 "d2@et.n")
-    (ds/add-identity node {:xt/id :dan} :id11 "text11")
-    (ds/add-identity node {:xt/id :dan} :id12 "text12")
-    (ds/add-identity node {:xt/id :dan2} :id21 "text21")
-    (ds/add-identity node {:xt/id :dan2} :id22 "text22")
+    (ds/add-identity node {:name :dan} :id11 "text11")
+    (ds/add-identity node {:name :dan} :id12 "text12")
+    (ds/add-identity node {:name :dan2} :id21 "text21")
+    (ds/add-identity node {:name :dan2} :id22 "text22")
     (testing "add and retrieve identities"
       (are [expected actual] (= expected actual)
-        (set [{:identity/text "text11"}
-              {:identity/text "text12"}])
+        (set [{:identity :id11 :text "text11"}
+              {:identity :id12 :text "text12"}])
         (set (ds/list-identities node {:xt/id :dan}))
-        (set [{:identity/text "text21"}
-              {:identity/text "text22"}])
+        (set [{:identity :id21 :text "text21"}
+              {:identity :id22 :text "text22"}])
         (set (ds/list-identities node {:xt/id :dan2}))))))
